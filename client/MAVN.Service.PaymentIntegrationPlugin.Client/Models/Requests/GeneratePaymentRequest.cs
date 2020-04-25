745379e7de0a203066161e7d9086c279e150eca6
Sergey Nesterov <sergey.nesterov@lykke.com>
2020-04-25 16:49:57 +0300

    GeneratePaymentRequest update

--- client/MAVN.Service.PaymentIntegrationPlugin.Client/Models/Requests/GeneratePaymentRequest.cs
+++ client/MAVN.Service.PaymentIntegrationPlugin.Client/Models/Requests/GeneratePaymentRequest.cs
@@ -8,16 +8,19 @@ namespace MAVN.Service.PaymentIntegrationPlugin.Client.Models.Requests
     [PublicAPI]
     public class GeneratePaymentRequest
     {
+        /// <summary>Payment request id</summary>
+        public string PaymentRequestId { get; set; }
+
         /// <summary>Amount</summary>
         public decimal Amount { get; set; }
 
         /// <summary>Currnecy</summary>
         public string Currency { get; set; }
 
-        /// <summary>SuccessRedirectUrlBase</summary>
-        public string SuccessRedirectUrlBase { get; set; }
+        /// <summary>SuccessRedirectUrl</summary>
+        public string SuccessRedirectUrl { get; set; }
 
-        /// <summary>SuccessRedirectUrlBase</summary>
-        public string FailRedirectUrlBase { get; set; }
+        /// <summary>SuccessRedirectUrl</summary>
+        public string FailRedirectUrl { get; set; }
     }
 }
